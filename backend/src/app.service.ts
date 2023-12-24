017b02ad2bc1ab2cfdbc10bdc2d054e22475236e
Tyutin Kirill <kerya.tyutin@yandex.ru>
2023-12-24 16:15:32 +0400

    1

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Request } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(request: Request): string {
-    if (process.env.sas) {
-      console.log(request);
-    }
+  getHello(): string {
     return 'Hello World!';
   }
 }
